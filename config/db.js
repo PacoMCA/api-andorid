a9d272f057a04c57a37d35ce1c0dc8cf9c7b9223
66973 <jo.cortes@mca.com.mx>
2023-01-11 16:05:46 -0600

    mongoose set

--- config/db.js
+++ config/db.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 
+mongoose.set("strictQuery", false);
 const connectDB = async ()=>{
     const conn = await mongoose.connect(process.env.MONGO_URI);
     console.log(`Connected succefull: ${conn.connection.host}`.cyan.bold);
